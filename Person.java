4b504af4e4f633d94a3fccccefd5afa8abe48a72
Sandul Wimalaweera <125640411+sandulwimalaweera@users.noreply.github.com>
2024-04-29 13:05:59 +0530

    create person class

--- Person.java
+++ Person.java
@@ -1,2 +1,41 @@
+import java.util.Date;
+
 public class Person {
+    private String name;
+    private String surname;
+    private Date dob;
+    private String mob;
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public Date getDob() {
+        return dob;
+    }
+
+    public void setDob(Date dob) {
+        this.dob = dob;
+    }
+
+    public String getMob() {
+        return mob;
+    }
+
+    public void setMob(String mob) {
+        this.mob = mob;
+    }
 }
